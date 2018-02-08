b07e79e5b2c46d618ba4f558fb80cd7e374ec3df
Andrew Erie <andrew_erie@dell.com>
2018-02-08 09:51:37 -0500

    Update

---
+++ number-value.js
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////
+// Custom Column - Format as Number w/ Commas
+////////////////////////////////////////////////////
+var numberColumn = React.createClass({
+    render() {
+        if (this.props.contentValue) {
+        	numvalue = Number(this.props.contentValue);
+        	return React.createElement('span', { className: "number-value"}, numvalue.toLocaleString());
+        }				        
+        return null;
+    }
+});
+
+manywho.component.register('num-column', numberColumn);
